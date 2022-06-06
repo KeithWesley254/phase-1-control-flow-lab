2d8c2e0ce39d83f43cf421c059b1a179c831ceda
Keith <keithwesley187@gmail.com>
2022-06-06 20:14:06 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if(ride<=400){
+    return'This one is on me!';
+  }
+  else if(ride>2000 && ride<=2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if(ride>2500){
+    return 'No can do.';
+  }
+  else{
+    return "input height in feet"
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  let demtips="gratifications";
+
+    switch(demtips){
+    case 'generous':
+      demtips='Thank you so much.';
+      break;
+    case 'not as generous':
+      demtips='Thank you.';
+      break;
+    default:
+      demtips='Bye.';
+      break;
+  }
+  return demtips
 }
